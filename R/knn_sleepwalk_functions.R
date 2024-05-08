83578d02122b32dcbe14f4929f4121fbdbddee74
Tyler Burns <burns.tyler@gmail.com>
2024-05-08 15:56:07 +0200

    modified the functions

--- R/knn_sleepwalk_functions.R
+++ R/knn_sleepwalk_functions.R
@@ -7,30 +7,30 @@
 #' @import sleepwalk
 NULL
 
-#' @title K-nearest neighbors Sleepwalk Direct
+#' @title K-nearest neighbors Sleepwalk
 #' @description Takes a data matrix and a 2-D embedding as input. It produces
 #' a 'KNN matrix' and places that along with the aforementioned inputs into the
 #' sleepwalk function. Unlike the default KNN sleepwalk function, this one makes
 #' two KNN matrices, like UMAP space vs high-D space for comparisons across the
 #' same embedding.
-#' @param mat1 A data matrix with data points as rows and features as columns.
-#' the matrix must already be filtered by the markers you care about.
-#' @param mat2 A second data matrix in the format as above
 #' @param embedding The 2-D embedding of the data matrix, in matrix format with
 #' data points as rows and two columns.
+#' @param orig_data A data matrix with data points as rows and features as columns.
+#' the matrix must already be filtered by the markers you care about.
 #' @param k The number of nearest neighbors to be visualized
 #' @param output_file The file to save your sleepwalk html page to
 #' @param point_size How big you want the point on the plots to be
 #' @param plot_names What you want the comparison plots to be named
 #' @export
-KnnSleepwalk <- function(mat1, mat2, embedding, k = 100, output_file = NULL, point_size = 1.5, plot_names = c("KNN embedding space", "KNN high-dim space")) {
+KnnSleepwalk <- function(embedding, orig_data, k = 100, output_file = NULL, point_size = 1.5, plot_names = c("KNN embedding space", "KNN high-dim space")) {
   message('Building distance matrix')
 
-  # First distance matrix
-  dist_mat1 <- dist(mat1) %>% as.matrix()
-  dist_mat2 <- dist(mat2) %>% as.matrix()
+  # Make distance matrices
+  dist_mat1 <- dist(embedding) %>% as.matrix()
+  dist_mat2 <- dist(orig_data) %>% as.matrix()
 
-  message("Finding k-nearest neighbors for mat1")
+  # KNN from the first distance matrix
+  message("Finding k-nearest neighbors for the embedding")
   nn_mat1 <- lapply(seq(nrow(dist_mat1)), function(i) {
     curr <- dist_mat1[i,]
     max_dist <- sort(curr, decreasing = FALSE)[k] # This is the K, decreasing set to false
@@ -38,9 +38,8 @@ KnnSleepwalk <- function(mat1, mat2, embedding, k = 100, output_file = NULL, poi
     return(curr)
   }) %>% do.call(rbind, .)
 
-  # Second distance matrix
-  dist_mat2 <- dist(mat2) %>% as.matrix()
-  message("Finding k-nearest neighbors for mat2")
+  # KNN from the second distance matrix
+  message("Finding k-nearest neighbors for original data")
   nn_mat2 <- lapply(seq(nrow(dist_mat2)), function(i) {
     curr <- dist_mat2[i,]
     max_dist <- sort(curr, decreasing = FALSE)[k] # This is the K, decreasing set to false
@@ -62,24 +61,23 @@ KnnSleepwalk <- function(mat1, mat2, embedding, k = 100, output_file = NULL, poi
 #' sleepwalk function. Unlike the default KFN sleepwalk function, this one makes
 #' two KFN matrices, like UMAP space vs high-D space for comparisons across the
 #' same embedding.
-#' @param mat1 A data matrix with data points as rows and features as columns.
-#' the matrix must already be filtered by the markers you care about.
-#' @param mat2 A second data matrix in the format as above
 #' @param embedding The 2-D embedding of the data matrix, in matrix format with
 #' data points as rows and two columns.
+#' @param orig_data A data matrix with data points as rows and features as columns.
+#' the matrix must already be filtered by the markers you care about.
 #' @param k The number of nearest neighbors to be visualized
 #' @param output_file The file to save your sleepwalk html page to
 #' @param point_size How big you want the point on the plots to be
 #' @param plot_names What you want the comparison plots to be named
 #' @export
-KfnSleepwalk <- function(mat1, mat2, embedding, k = 100, output_file = NULL, point_size = 1.5, plot_names = c("KFN embedding space", "KFN high-dim space")) {
+KfnSleepwalk <- function(embedding, orig_data, embedding, k = 100, output_file = NULL, point_size = 1.5, plot_names = c("KFN embedding space", "KFN high-dim space")) {
   message('Building distance matrix')
 
   # First distance matrix
-  dist_mat1 <- dist(mat1) %>% as.matrix()
-  dist_mat2 <- dist(mat2) %>% as.matrix()
+  dist_mat1 <- dist(embedding) %>% as.matrix()
+  dist_mat2 <- dist(orig_data) %>% as.matrix()
 
-  message("Finding k-nearest neighbors for mat1")
+  message("Finding k-nearest neighbors for the embedding")
   nn_mat1 <- lapply(seq(nrow(dist_mat1)), function(i) {
     curr <- dist_mat1[i,]
     min_dist <- sort(curr, decreasing = TRUE)[k] # This is the K, decreasing set to true
@@ -88,8 +86,7 @@ KfnSleepwalk <- function(mat1, mat2, embedding, k = 100, output_file = NULL, poi
   }) %>% do.call(rbind, .)
 
   # Second distance matrix
-  dist_mat2 <- dist(mat2) %>% as.matrix()
-  message("Finding k-nearest neighbors for mat2")
+  message("Finding k-nearest neighbors for the original data")
   nn_mat2 <- lapply(seq(nrow(dist_mat2)), function(i) {
     curr <- dist_mat2[i,]
     min_dist <- sort(curr, decreasing = TRUE)[k] # This is the K, decreasing set to false
