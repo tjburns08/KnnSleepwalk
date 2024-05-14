630254d1076f90e8940877aa7a55b0dc1530e8dd
Tyler Burns <burns.tyler@gmail.com>
2024-05-14 20:22:18 +0200

    re-added import statements into the main function's code

--- R/knn_sleepwalk_functions.R
+++ R/knn_sleepwalk_functions.R
@@ -2,6 +2,11 @@
 # Author: Tyler J Burns
 # Date: May 7, 2024
 
+#' @importFrom magrittr %>%
+#' @import lsa
+#' @import sleepwalk
+NULL
+
 #' @title Make nearest (or farthest) neighbor matrix
 #' @description A helper function for KnnSleepwalk which takes a matrix as
 #' input, computes the distance matrix, and then changes the values such that
